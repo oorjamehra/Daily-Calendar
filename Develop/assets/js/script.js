86aeb61f3ae1bc2906631b798f0bc7f9c98abf2d
Oorja Mehra <oorjamehra@gmail.com>
2022-04-15 20:42:15 -0500

    add time blocks in .js

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,96 @@
+// create time blocks for scheduling reminders
+var myCalendar = [
+    {
+        id: "0",
+        hour: "07",
+        time: "07",
+        meridiem: "am",
+        task: ""
+    },
+    {
+        id: "1",
+        hour: "08",
+        time: "08",
+        meridiem: "am",
+        task: ""
+    },
+    {
+        id: "2",
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        task: ""
+    },
+    {
+        id: "3",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        task: ""
+    },
+    {
+        id: "4",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        task: ""
+    },
+    {
+        id: "5",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        task: ""
+    },
+    {
+        id: "6",
+        hour: "01",
+        time: "13",
+        meridiem: "pm",
+        task: ""
+    },
+    {
+        id: "7",
+        hour: "02",
+        time: "14",
+        meridiem: "pm",
+        task: ""
+    },
+    {
+        id: "8",
+        hour: "03",
+        time: "15",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "9",
+        hour: "04",
+        time: "16",
+        meridiem: "pm",
+        task: ""
+    },
+    {
+        id: "10",
+        hour: "05",
+        time: "17",
+        meridiem: "pm",
+        task: ""
+    },
+    {
+        id: "11",
+        hour: "06",
+        time: "18",
+        meridiem: "pm",
+        task: ""
+
+    },
+    {
+        id: "12",
+        hour: "07",
+        time: "19",
+        meridiem: "pm",
+        task: ""
+
+    },
+]
